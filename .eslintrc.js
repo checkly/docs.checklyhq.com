631d6437ad0318e332d646659af84b372fcdcdf2
Ignacio Anaya <nacho@checklyhq.com>
2021-06-22 11:53:37 -0300

    refactor: update eslint and fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
   parser: 'babel-eslint',
@@ -9,19 +7,17 @@ module.exports = {
   env: {
     browser: true,
   },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
   extends: 'standard',
-  // required to lint *.vue files
   plugins: [
     'html'
   ],
-  // add your custom rules here
-  'rules': {
-    // allow paren-less arrow functions
+  globals: {
+    $: 'readonly',
+    X2JS: 'readonly'
+  },
+  rules: {
     'arrow-parens': 0,
-    // allow async-await
     'generator-star-spacing': 0,
-    // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -61,7 +61,7 @@ KKKN|*5KKKKKKKKKKKKKKKKPL**''''_ __  ''''*|5$KKKKKKKKKKKKKKKBPLL#BKBLL*|$#KM'
  */
 
 $(document).ready(() => {
-  if($('body').hasClass('landing')){
+  if ($('body').hasClass('landing')) {
     fetch('https://api.checklyhq.com/public-stats')
       .then(function (response) {
         return response.json()
@@ -73,7 +73,6 @@ $(document).ready(() => {
         $('#browser-check-results').text(countBrowser)
       })
   }
-
 })
 
 /**
@@ -81,84 +80,83 @@ $(document).ready(() => {
 */
 
 $(document).ready(() => {
-  if($('div').hasClass('opensource')){
-    const xhr = new XMLHttpRequest();
+  if ($('div').hasClass('opensource')) {
+    const xhr = new XMLHttpRequest()
 
-    xhr.onload = function() {
-      var x2js = new X2JS();
-      const  data = x2js.xml2json(xhr.responseXML.documentElement);
-      var titleId = '';
-      var descId = '';
-      var linkId = '';
-      data.channel.item.forEach(function(node, i) {
+    xhr.onload = function () {
+      var x2js = new X2JS()
+      const data = x2js.xml2json(xhr.responseXML.documentElement)
+      var titleId = ''
+      var descId = ''
+      var linkId = ''
+      data.channel.item.forEach(function (node, i) {
         if (i < 6) {
-          titleId ='#card-'+(i+1)+'-title';
-          descId = '#card-'+(i+1)+'-description';
-          linkId = '#card-'+(i+1)+'-link';
-          $(titleId).text(node.title);
-          $(descId).text(node.description);
-          $(linkId).attr('href', node.link);
+          titleId = '#card-' + (i + 1) + '-title'
+          descId = '#card-' + (i + 1) + '-description'
+          linkId = '#card-' + (i + 1) + '-link'
+          $(titleId).text(node.title)
+          $(descId).text(node.description)
+          $(linkId).attr('href', node.link)
         }
       })
     }
 
-    xhr.open("GET", "https://theheadless.dev/rss.xml");
-    xhr.responseType = "document";
-    xhr.send();
+    xhr.open('GET', 'https://theheadless.dev/rss.xml')
+    xhr.responseType = 'document'
+    xhr.send()
 
-    fetch("https://api.github.com/repos/checkly/headless-recorder")
+    fetch('https://api.github.com/repos/checkly/headless-recorder')
       .then(function (response) {
         return response.json()
       })
       .then(function (res) {
         let stars = ''
         if (res.stargazers_count > 1000) {
-          stars = (res.stargazers_count/1000).toFixed(1) + "k"
+          stars = (res.stargazers_count / 1000).toFixed(1) + 'k'
         } else {
           stars = res.stargazers_count
         }
-        $('#headless-recorder').text(stars);
+        $('#headless-recorder').text(stars)
       })
-    fetch("https://api.github.com/repos/checkly/theheadless.dev")
+    fetch('https://api.github.com/repos/checkly/theheadless.dev')
       .then(function (response) {
         return response.json()
       })
       .then(function (res) {
         let stars = ''
         if (res.stargazers_count > 1000) {
-          stars = (res.stargazers_count/1000).toFixed(1) + "k"
+          stars = (res.stargazers_count / 1000).toFixed(1) + 'k'
         } else {
           stars = res.stargazers_count
         }
-        $('#theheadless-dev').text(stars);
+        $('#theheadless-dev').text(stars)
       })
-    fetch("https://api.github.com/repos/checkly/terraform-provider-checkly")
+    fetch('https://api.github.com/repos/checkly/terraform-provider-checkly')
       .then(function (response) {
         return response.json()
       })
       .then(function (res) {
         let stars = ''
         if (res.stargazers_count > 1000) {
-          stars = (res.stargazers_count/1000).toFixed(1) + "k"
+          stars = (res.stargazers_count / 1000).toFixed(1) + 'k'
         } else {
           stars = res.stargazers_count
         }
-        $('#terraform-provider').text(stars);
+        $('#terraform-provider').text(stars)
       })
-    fetch("https://api.github.com/repos/checkly/jamstack-deploy")
+    fetch('https://api.github.com/repos/checkly/jamstack-deploy')
       .then(function (response) {
         return response.json()
       })
       .then(function (res) {
         let stars = ''
         if (res.stargazers_count > 1000) {
-          stars = (res.stargazers_count/1000).toFixed(1) + "k"
+          stars = (res.stargazers_count / 1000).toFixed(1) + 'k'
         } else {
           stars = res.stargazers_count
         }
-        $('#jamstack-deploy').text(stars);
+        $('#jamstack-deploy').text(stars)
       })
-
   }
 })
 
@@ -166,21 +164,21 @@ $(document).ready(() => {
  * Hover Dropdown
  */
 
-$(document).ready(function() {
-  $('#dropdown-li').mouseenter(function(){
-    $('#dropdown-li').addClass('show');
+$(document).ready(function () {
+  $('#dropdown-li').mouseenter(function () {
+    $('#dropdown-li').addClass('show')
   })
-  $('#dropdown-menu, #dropdown-li').mouseleave(function() {
-    $('#dropdown-li').removeClass('show');
+  $('#dropdown-menu, #dropdown-li').mouseleave(function () {
+    $('#dropdown-li').removeClass('show')
   })
 })
 
-$(document).ready(function() {
-  $('#dropdown-li-learn').mouseenter(function(){
-    $('#dropdown-li-learn').addClass('show');
+$(document).ready(function () {
+  $('#dropdown-li-learn').mouseenter(function () {
+    $('#dropdown-li-learn').addClass('show')
   })
-  $('#dropdown-menu, #dropdown-li-learn').mouseleave(function() {
-    $('#dropdown-li-learn').removeClass('show');
+  $('#dropdown-menu, #dropdown-li-learn').mouseleave(function () {
+    $('#dropdown-li-learn').removeClass('show')
   })
 })
 
@@ -195,24 +193,23 @@ $(document).ready(() => {
   toggle.click(() => {
     if (toggleFlag === 0) {
       toggleCard.css({
-        display: 'block',
+        display: 'block'
       })
       toggleArrow.css({
-        transform: 'rotate(180deg)',
+        transform: 'rotate(180deg)'
       })
       toggleFlag = 1
     } else {
       toggleCard.css({
-        display: 'none',
+        display: 'none'
       })
       toggleArrow.css({
-        transform: 'rotate(0)',
+        transform: 'rotate(0)'
       })
       toggleFlag = 0
     }
   })
 })
-
 
 /**
  * START Pricing
@@ -270,44 +267,41 @@ $(document).ready(() => {
  * END Pricing
  */
 
-
 /**
  * Integration tabs
  */
 
 $(document).ready(() => {
-  $('#integration-tab-0').addClass('active');
-  $('#integration-tab-0-content').addClass('active');
-  var integrationTab = $('.integration-tab');
-  var tabContent = $('.integration-tab-content');
+  $('#integration-tab-0').addClass('active')
+  $('#integration-tab-0-content').addClass('active')
+  var integrationTab = $('.integration-tab')
+  var tabContent = $('.integration-tab-content')
 
-  integrationTab.click(function() {
-    var id = $(this).attr('id');
-    var content_id = '#' + id + '-content'
+  integrationTab.click(function () {
+    var id = $(this).attr('id')
+    var conentId = '#' + id + '-content'
     $(integrationTab).removeClass('active')
     $(tabContent).removeClass('active')
     $(this).addClass('active')
-    $(content_id).addClass('active')
+    $(conentId).addClass('active')
   })
 })
 
-
-
- /**
+/**
  *Footer DropDown in Tablet&Mobile
  */
- $(document).ready(() => {
-  var menuHeader = $('.menu_header');
-  menuHeader.click(function() {
-    var id = $(this).attr('id');
-    const menu_header_id = '#' + id
-    const menu_id = '#' + id + '_drop'
-    if ($(menu_id).attr('class') === 'desktop-show') {
-      $(menu_id).addClass('show')
-      $(menu_header_id).addClass('rotate')
+$(document).ready(() => {
+  var menuHeader = $('.menu_header')
+  menuHeader.click(function () {
+    var id = $(this).attr('id')
+    const menuHeaderId = '#' + id
+    const menuId = '#' + id + '_drop'
+    if ($(menuId).attr('class') === 'desktop-show') {
+      $(menuId).addClass('show')
+      $(menuHeaderId).addClass('rotate')
     } else {
-      $(menu_id).removeClass('show')
-      $(menu_header_id).removeClass('rotate')
+      $(menuId).removeClass('show')
+      $(menuHeaderId).removeClass('rotate')
     }
   })
 })
