148ba3edcbb9808eea101be7d8ea491d2622ef7d
Tim Nolet <tnolet@users.noreply.github.com>
2022-06-06 17:17:30 +0200

    style: some linting fixes (sc-9201) (#437)

---
+++ _checkly/pricing.check.js
@@ -0,0 +1,20 @@
+const expect = require('expect')
+const { chromium } = require('playwright')
+
+async function run () {
+  const browser = await chromium.launch()
+  const page = await browser.newPage()
+  const baseUrl = process.env.ENVIRONMENT_URL || 'https://checklyhq.com'
+  const targetUrl = baseUrl + '/pricing/'
+  console.log('Visiting', targetUrl)
+  await page.goto(targetUrl)
+  const title = await page.title()
+
+  await page.screenshot({ path: 'pricing.png' })
+
+  expect(title).toContain('Pricing | Checkly')
+  await page.close()
+  await browser.close()
+}
+
+run()
