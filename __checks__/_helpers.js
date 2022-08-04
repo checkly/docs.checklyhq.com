e41968d1e20e14a6eaeede096f17494a8a9ed56d
stefan judis <stefanjudis@gmail.com>
2022-08-04 16:07:25 +0200

    feat: add new Checkly tests

---
+++ __checks__/_helpers.js
@@ -0,0 +1,27 @@
+const { chromium } = require('playwright')
+const checklyConfig = require('../checkly.config.js')
+
+async function goToPage (path) {
+  const browser = await chromium.launch({ headless: checklyConfig.headless })
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  await page.setViewportSize(checklyConfig.defaultViewPortSize)
+  await page.goto(checklyConfig.baseURL + path)
+
+  return { page, browser }
+}
+
+function screenshot (page, name) {
+  return page.screenshot({ path: `./__checks__/screenshots/${name}.jpg` })
+}
+
+async function cleanUp (page, browser) {
+  await page.close()
+  await browser.close()
+}
+
+module.exports = {
+  goToPage,
+  screenshot,
+  cleanUp
+}
