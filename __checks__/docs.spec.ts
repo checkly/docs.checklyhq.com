9bd50afdcc9d9a67645b6ac993f0589e7f4abd73
tnolet <tim@checklyhq.com>
2023-05-09 17:37:06 +0200

    chore: update tests

--- __checks__/docs.spec.ts
+++ __checks__/docs.spec.ts
@@ -3,7 +3,7 @@ import { ChecklySitePage } from './poms/ChecklySitePage'
 
 test('homepage', async ({ page }) => {
   const checklyPage = new ChecklySitePage(page)
-  await checklyPage.goto()
-  await checklyPage.screenshot('homepage')
-  expect(await page.title()).toEqual('Build and Run Synthetics That Scale')
+  await checklyPage.goto('/docs')
+  await checklyPage.screenshot('docs')
+  expect(await page.title()).toEqual('Introduction to Checkly | Checkly')
 })
