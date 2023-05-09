ab1a4d7c75347f85235fb9324696b853ba523b03
Nico Domino <yo@ndo.dev>
2023-05-09 15:50:11 +0200

    fix: 'checklyhq.com' related MaC checks for new marketing site '[sc-00] (#799)

--- __checks__/home.spec.ts
+++ __checks__/home.spec.ts
@@ -5,5 +5,5 @@ test('homepage', async ({ page }) => {
   const checklyPage = new ChecklySitePage(page)
   await checklyPage.goto()
   await checklyPage.screenshot('homepage')
-  expect(await page.title()).toEqual('Build and Run Synthetics That Scale')
+  expect(await page.title()).toEqual('Code, test, and deploy synthetic monitoring at scale.')
 })
--- __checks__/pricing.spec.ts
+++ __checks__/pricing.spec.ts
@@ -1,14 +1,20 @@
 import { test, expect } from '@playwright/test'
 import { ChecklySitePage } from './poms/ChecklySitePage'
 
-test('pricing', async ({ page }) => {
+test('pricing', async ({ page, context }) => {
   const checklyPage = new ChecklySitePage(page)
   await checklyPage.goto('/pricing')
   await checklyPage.screenshot('pricing')
 
-  expect(await page.title()).toEqual('Pricing | Checkly')
-  await page.locator('[data-test="pricing-free-signup"]').click()
-  await page.waitForNavigation({ url: 'https://auth.checklyhq.com/**' })
+  expect(await page.title()).toEqual('Checkly pricing plans')
 
+  // Last is required because otherwise this resolves to two links, one in the free plan
+  // card and another one in the navbar
+  await page.getByRole('link', { name: 'Start for free' }).last().click()
+
+  // Waiting for click above to open window in new tab
+  const newTabPage = await context.waitForEvent('page')
+
+  expect(newTabPage.url()).toContain('app.checklyhq.com/signup')
   await checklyPage.screenshot('signup')
 })
