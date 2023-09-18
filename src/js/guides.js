94d17e4fb9b91ba714899eb8783904f4e501d559
Pilar Martinez <pilar@checklyhq.com>
2023-09-18 14:52:48 +0200

    fix: add fixed sidebar [sc-17693]

--- src/js/guides.js
+++ src/js/guides.js
@@ -13,23 +13,3 @@ $(document).ready(() => {
     })
   }
 })
-
-/**
- * Sidemenu fixed position after some scroll-up
- */
-
-const sideMenuDistance = $('#tocMenu').offset().top - 10
-
-$(window).on('scroll', function () {
-  if ($(window).scrollTop() >= sideMenuDistance) {
-    $('#tocMenu').css({
-      position: 'fixed',
-      top: '30px'
-    })
-  } else {
-    $('#tocMenu').css({
-      position: 'relative',
-      top: '0'
-    })
-  }
-})
