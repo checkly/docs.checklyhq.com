1dc41534550c1227691c29b94706e58e0e943733
tnolet <tim@checklyhq.com>
2023-09-20 13:47:15 +0200

    test: explicitly block LinkedIn

--- __checks__/poms/ChecklySitePage.ts
+++ __checks__/poms/ChecklySitePage.ts
@@ -9,8 +9,12 @@ export class ChecklySitePage {
   }
 
   async goto (uri = '/') {
+    // block linkedin pixel as it can hang
+    await this.page.route(/.*px\.ads\.linkedin.*/, (route) => {
+      route.abort();
+    });
     await this.page.setViewportSize(defaults.playwright.viewportSize)
-    await this.page.goto(defaults.baseURL + uri, { waitUntil: 'domcontentloaded' })
+    await this.page.goto(defaults.baseURL + uri)
   }
 
   async screenshot (name) {
