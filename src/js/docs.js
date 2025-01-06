b873218f45ebf1a9e297369ed6d5ab7b3ec51f70
tnolet <tim@checklyhq.com>
2025-01-06 16:05:28 +0100

    fix: remove unused code. Fix errors on non-existing menus

--- src/js/docs.js
+++ src/js/docs.js
@@ -87,7 +87,9 @@ let sideMenuDistance
 
 if (sideMenu.length) {
   sideMenuDistance = sideMenu.offset().top - 10
-} else { sideMenuDistance = tocMenu.offset().top - 10 }
+} else if (tocMenu.length) {
+  sideMenuDistance = tocMenu.offset().top - 10
+}
 
 $(window).on('scroll', function () {
   if ($(window).scrollTop() >= sideMenuDistance) {
--- src/js/guides.js
+++ src/js/guides.js
@@ -2,18 +2,22 @@
  * Docs TOC Sidebar
  */
 
-const sideMenuDistance = $('#tocMenu').offset().top - 10
+$(document).ready(function () {
+  const tocMenu = $('#tocMenu')
+  if (!tocMenu.length) return
+  const sideMenuDistance = tocMenu.offset().top - 10
 
-$(window).on('scroll', function () {
-  if ($(window).scrollTop() >= sideMenuDistance) {
-    $('#tocMenu').css({
-      position: 'fixed',
-      top: '30px'
-    })
-  } else {
-    $('#tocMenu').css({
-      position: 'relative',
-      top: '0'
-    })
-  }
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() >= sideMenuDistance) {
+      tocMenu.css({
+        position: 'fixed',
+        top: '30px'
+      })
+    } else {
+      tocMenu.css({
+        position: 'relative',
+        top: '0'
+      })
+    }
+  })
 })
