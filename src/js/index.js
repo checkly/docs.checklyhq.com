fb680506141f0d5d238f28b55e64aa1b5961d9ec
tnolet <tim@checklyhq.com>
2025-01-29 16:50:18 +0100

    docs: updates some docs based on customer discussion.

--- src/js/index.js
+++ src/js/index.js
@@ -62,3 +62,9 @@ async function copyCodeBlockExecCommand (codeToCopy) {
     console.error('Failed to copy text: ', err)
   }
 }
+
+function fectchMeFromApp () {
+  console.log('fectchMeFromApp')
+}
+
+fectchMeFromApp()
